a86796cfa3375d6f3b6b05a9f8c8c1b3683457a3
Phan Minh Hoang <hoangpmhe130495@fpt.edu.vn>
2020-10-29 13:50:23 +0700

    Integrated Firebase Google

--- app/src/main/java/com/example/h3t_project/MainActivity.java
+++ app/src/main/java/com/example/h3t_project/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.h3t_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editName;
+    private EditText editPrice;
+    private Button add;
+    private FirebaseFirestore firestore;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editName = findViewById(R.id.name);
+        editPrice = findViewById(R.id.price);
+        add = findViewById(R.id.add);
+        firestore  = FirebaseFirestore.getInstance();
+        final CollectionReference reference = firestore.collection("product");
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Map<String, Object> product = new HashMap<>();
+                product.put("name", editName.getText().toString());
+                product.put("price", editPrice.getText().toString());
+                reference.add(product).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(MainActivity.this,"Add Ok !", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,"Add Failed ! ", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }
