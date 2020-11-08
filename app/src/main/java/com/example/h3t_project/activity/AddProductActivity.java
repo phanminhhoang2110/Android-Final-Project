dc8604948c581f7b61c7081d66acef49a2f2983f
thanh <tranthanhtung110699@gmail.com>
2020-11-08 21:49:38 +0700

    sua ten file

--- app/src/main/java/com/example/h3t_project/activity/AddProductActivity.java
+++ app/src/main/java/com/example/h3t_project/activity/AddProductActivity.java
@@ -1,8 +1,6 @@
 package com.example.h3t_project.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
@@ -13,8 +11,6 @@ import com.example.h3t_project.adapter.FragmentImage;
 import com.example.h3t_project.adapter.FragmentShort;
 import com.example.h3t_project.adapter.ViewPagerAdapter;
 import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
 
 public class AddProductActivity extends AppCompatActivity {
 
