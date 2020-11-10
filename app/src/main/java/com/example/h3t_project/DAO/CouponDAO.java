16c902fbfb1c8f73b303d9e37716abecc4dfce05
Phan Minh Hoang <hoangpmhe130495@fpt.edu.vn>
2020-11-10 10:37:04 +0700

    Delete Coupon From DB

--- app/src/main/java/com/example/h3t_project/DAO/CouponDAO.java
+++ app/src/main/java/com/example/h3t_project/DAO/CouponDAO.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class CouponDAO extends DatabaseManager {
   PreparedStatement preparedStatement;
   ResultSet resultSet = null;
+
   public boolean addCoupon(CouponItem couponItem) {
     try {
       String query = "INSERT INTO [dbo].[tbl_coupons] VALUES ( ?, ? )";
@@ -26,7 +27,7 @@ public class CouponDAO extends DatabaseManager {
     return false;
   }
 
-  public List<CouponItem> getCoupons(){
+  public List<CouponItem> getCoupons() {
     List<CouponItem> couponItems = null;
     try {
       String query = "SELECT [code],[value] FROM [dbo].[tbl_coupons]";
@@ -34,16 +35,30 @@ public class CouponDAO extends DatabaseManager {
       preparedStatement = connection.prepareStatement(query);
       resultSet = preparedStatement.executeQuery();
       couponItems = new ArrayList<>();
-      while (resultSet.next()){
+      while (resultSet.next()) {
         CouponItem couponItem = new CouponItem();
         couponItem.setCodeCoupon(resultSet.getString("code"));
         int valueCoupon = Integer.parseInt(resultSet.getString("value"));
         couponItem.setValueCoupon(valueCoupon);
         couponItems.add(couponItem);
       }
-    }catch (Exception e){
+    } catch (Exception e) {
       e.printStackTrace();
     }
     return couponItems;
+  }
+
+  public int deleteCoupon(String code) {
+    try {
+      String query = "DELETE FROM [dbo].[tbl_coupons] WHERE code = ?";
+      connection = connect();
+      preparedStatement = connection.prepareStatement(query);
+      preparedStatement.setString(1,code);
+      int rowAffected = preparedStatement.executeUpdate();
+      return rowAffected;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return 0;
   }
 }
