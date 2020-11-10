656d99c72b9fa9d565e5f9f6710bbf9e4b78c8df
thanh <tranthanhtung110699@gmail.com>
2020-11-10 10:51:07 +0700

    Commit customer order

---
+++ app/src/main/java/com/example/h3t_project/activity/CustomerListOrderActivity.java
@@ -0,0 +1,25 @@
+package com.example.h3t_project.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.example.h3t_project.R;
+
+public class CustomerListOrderActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_list_order);
+
+        toolbar = findViewById(R.id.cusactionbar);
+
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+}
