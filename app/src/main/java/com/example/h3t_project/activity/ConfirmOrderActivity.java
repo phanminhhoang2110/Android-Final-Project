15656dfb456c965fdfdbfcd7a2f84af005090dbc
Phan Minh Hoang <hoangpmhe130495@fpt.edu.vn>
2020-11-18 12:21:30 +0700

    Fix orderDetailCòmirm

--- app/src/main/java/com/example/h3t_project/activity/ConfirmOrderActivity.java
+++ app/src/main/java/com/example/h3t_project/activity/ConfirmOrderActivity.java
@@ -1,26 +1,29 @@
 package com.example.h3t_project.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.os.Bundle;
 import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.h3t_project.R;
 
 public class ConfirmOrderActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_order);
-        toolbar = findViewById(R.id.actionbar3);
+  Toolbar toolbar;
 
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_confirm_order);
+    toolbar = findViewById(R.id.toolbar);
 
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    setSupportActionBar(toolbar);
 
-        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
-    }
+    getSupportActionBar().setTitle("Xác nhận đơn hàng");
+
+    getSupportActionBar().setDisplayShowHomeEnabled(true);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+  }
 }
